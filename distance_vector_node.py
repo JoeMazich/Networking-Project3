c559312b3b91aec777c3dbeee9c61f8999509d7c
Crush302 <mazich.joe@gmail.com>
2020-11-05 18:51:04 -0600

    Added routing table class for data structure

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -5,9 +5,10 @@ class Distance_Vector_Node(Node):
     def __init__(self, id):
         super().__init__(id)
 
-    # Return a string
+        self.routing_table = RoutingTable(id)
+
     def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+        return 'LS node: %s\nRT: %s\n' % (self.id, self.RT)
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
@@ -20,4 +21,26 @@ class Distance_Vector_Node(Node):
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
-        return -1
+        try:
+            hops = self.self.routing_table.hops[destination]
+        except:
+            return -1
+        else:
+            return hops[0]
+
+class RoutingTable:
+
+    def __init__(self, id: int):
+        self.cost = {id: 0}
+        self.hops = {id: None}
+
+    def __repr__(self):
+        return '%s~%s' % (json.dumps(self.cost), json.dumps(self.hops))
+
+    def update(self, id: int, total_latency: int, path: list):
+        self.hops[id] = path
+        self.cost[id] = total_latency
+
+    def delete(self, id: int):
+        del self.hops[id]
+        del self.cost[id]
