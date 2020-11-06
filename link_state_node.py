8c99a359df33de21ce2543f95570633e8f989591
Crush302 <mazich.joe@gmail.com>
2020-11-05 18:44:21 -0600

    check

--- link_state_node.py
+++ link_state_node.py
@@ -6,38 +6,33 @@ class Link_State_Node(Node):
         super().__init__(id)
         self.full_graph = [] #every edge in the graph represented as a list of tuples in the form (node1, node2, cost)
 
-        self.hops = {id: None}
-        self.costs = {id: 0}
-
     # Return a string
     def __str__(self):
         return "A Link-state Node: " + str(self.id) + "\n"
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
-        # latency = -1 if delete a link
+        tuple = (self.id, neighbor, latency)
+        for i, edge in enumerate(self.full_graph):
+            if (edge[0] == tuple[0] and edge[1] == tuple[1]) or (edge[0] == tuple[1] and edge[1] == tuple[0]):
+                del self.full_graph[i]
 
-        if latency == -1 and neighbor in self.neighbors:
-            self.neighbors.remove(neighbor)
-            del self.hops[neighbor]
-            del self.costs[neighbor]
-
-        elif neighbor not in self.neighbors:
-            self.neighbors.append(neighbor)
-            self.hops[neighbor] = [neighbor]
-            self.costs[neighbor] = latency
-
-        # self.send_to_neighbors all of the updates recieved
-
+        self.full_graph.append(tuple)
+        self.send_to_neighbors(self.full_graph)
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
         # parse out the recieved updates
-        print(self.id, ' recieved: ', m)
+        for edge in m:
+            for i, eddge in enumerate(self.full_graph):
+                if (edge[0] == eddge[0] and edge[1] == eddge[1]) or (edge[0] == eddge[1] and edge[1] == eddge[0]):
+                    del self.full_graph[i]
+            self.full_graph.append(edge)
 
     # Return a neighbor, -1 if no path to destination
-    def get_next_hop(self, destination): 
-        if self.id == destination.id:
+    def get_next_hop(self, destination):
+        print(self.full_graph)
+        if self.id == destination:
             return self #Not actually sure what to do in this case
         distances = {self: 0}
         previous = {self: None}
@@ -77,4 +72,3 @@ def get_latency(node1, node2, graph):
         if edge[0].id == node2.id and edge[1].id == node1.id:
             return edge[2]
     raise ValueError("Should never see this")
-
